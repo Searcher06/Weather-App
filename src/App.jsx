eb31707a07946d0697dc7cd70d23d96cfa591758
Searcher06 <ahmadibrahimsearcher@example.com>
2024-12-31 02:59:01 +0100

    api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
 function App() {
+    useEffect(()=>{
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=kano&appid=54cb4436af4623404812d9c1bf43568e')
+       .then(response => response.json()).then(data => console.log(data))
+    })
     return (
         <></>
     );
